1973aa8308843ec88d58ec2072991c6490674058
Kunal628-hue <creationofkunal@gmail.com>
2026-01-18 17:53:03 +0530

    Fix Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/personal-portfolio/" // 👈 EXACT GitHub repo name
+  base: "/" // or remove base entirely
 });
